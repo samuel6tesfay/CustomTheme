561a0914b7a4b83501fdbe3a9312e87bec76867e
samuel <samuel6tesfay@gmail.com>
2022-01-12 01:18:22 +0300

    updating page

---
+++ page.php
@@ -0,0 +1,18 @@
+<?php 
+// adds default header or from header.php
+get_header();
+?>
+<div class="container">
+<?php
+if(have_posts()){
+    while(have_posts()){
+        the_post()?>
+        <h1> <a href="<?php the_permalink()?>"><?php the_title()?></a></h1>
+        <?php the_content();
+     }}?>
+</div>
+<?php
+// adds default footer or from footer.php
+get_footer();
+  
+?>
